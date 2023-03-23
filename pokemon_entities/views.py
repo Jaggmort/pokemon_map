5c84b90776e03a35a4091ab2fa692deed6cc20c4
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-03-24 00:24:55 +0300

    show pokemons on mainpage map.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -30,7 +30,6 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 def show_all_pokemons(request):
     pokemons = PokemonEntity.objects.all() 
-    url = 'https://upload.wikimedia.org/wikipedia/ru/c/ca/%D0%91%D1%83%D0%BB%D1%8C%D0%B1%D0%B0%D0%B7%D0%B0%D0%B2%D1%80.png'
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
 
     for pokemon in pokemons:
