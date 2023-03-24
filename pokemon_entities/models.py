07bfc37f1b4d99d78d1bf20f977a00ef6a4fdb8a
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-03-24 04:39:21 +0300

    Added field naming in russian

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,28 +2,28 @@ from django.db import models
 from django.utils import timezone
 
 class Pokemon(models.Model):
-    title = models.TextField()
-    image = models.ImageField(null=True)
-    description = models.TextField(null=True)
-    title_en = models.TextField(null=True)
-    title_jp = models.TextField(null=True)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, related_name='child')
+    title = models.TextField('Название')
+    image = models.ImageField('Изображение', null=True)
+    description = models.TextField('Описание', null=True)
+    title_en = models.TextField('Название на английском', null=True)
+    title_jp = models.TextField('Название на японском',null=True)
+    parent = models.ForeignKey('self', verbose_name= 'Из кого появился',on_delete=models.CASCADE, null=True, related_name='child')
 
     def __str__(self):
         return self.title
      
     
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)    
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField(default=timezone.now())
-    disappeared_at = models.DateTimeField(default=timezone.now())
-    level = models.IntegerField(default=0)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defence = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.CASCADE)    
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+    appeared_at = models.DateTimeField('Появиться в', default=timezone.now())
+    disappeared_at = models.DateTimeField('Исчезнет в', default=timezone.now())
+    level = models.IntegerField('Уровень', default=0)
+    health = models.IntegerField('Жизни', default=0)
+    strength = models.IntegerField('Сила', default=0)
+    defence = models.IntegerField('Защита', default=0)
+    stamina = models.IntegerField('Выносливость', default=0)
 
     def get_lat(self):
         return self.lat
