4b3c7e4fa0c955b932d3db5431943066c65db575
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-03-24 04:51:44 +0300

    Added null ability for not necessary fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 
 class Pokemon(models.Model):
     title = models.TextField('Название')
-    image = models.ImageField('Изображение', null=True)
-    description = models.TextField('Описание', null=True)
-    title_en = models.TextField('Название на английском', null=True)
-    title_jp = models.TextField('Название на японском',null=True)
-    parent = models.ForeignKey('self', verbose_name= 'Из кого появился',on_delete=models.CASCADE, null=True, related_name='child')
+    image = models.ImageField('Изображение', null=True, blank=True)
+    description = models.TextField('Описание', blank=True)
+    title_en = models.TextField('Название на английском', blank=True)
+    title_jp = models.TextField('Название на японском',blank=True)
+    parent = models.ForeignKey('self', verbose_name= 'Из кого появился',on_delete=models.CASCADE, null=True, blank=True, related_name='child')
 
     def __str__(self):
         return self.title
@@ -15,15 +15,15 @@ class Pokemon(models.Model):
     
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.CASCADE)    
-    lat = models.FloatField('Широта')
-    lon = models.FloatField('Долгота')
-    appeared_at = models.DateTimeField('Появиться в', default=timezone.now())
-    disappeared_at = models.DateTimeField('Исчезнет в', default=timezone.now())
-    level = models.IntegerField('Уровень', default=0)
-    health = models.IntegerField('Жизни', default=0)
-    strength = models.IntegerField('Сила', default=0)
-    defence = models.IntegerField('Защита', default=0)
-    stamina = models.IntegerField('Выносливость', default=0)
+    lat = models.FloatField('Широта', null=True)
+    lon = models.FloatField('Долгота', null=True)
+    appeared_at = models.DateTimeField('Появиться в', default=timezone.now(), null=True)
+    disappeared_at = models.DateTimeField('Исчезнет в', default=timezone.now(), null=True)
+    level = models.IntegerField('Уровень', default=0, null=True, blank=True)
+    health = models.IntegerField('Жизни', default=0, null=True, blank=True)
+    strength = models.IntegerField('Сила', default=0, null=True, blank=True)
+    defence = models.IntegerField('Защита', default=0, null=True, blank=True)
+    stamina = models.IntegerField('Выносливость', default=0, null=True, blank=True)
 
     def get_lat(self):
         return self.lat
